5d6fea9be43defa16f3672c7fafddce63131f2f9
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-01-31 17:27:50 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // accept two int numbers and find the maximum then display
+            int fno;
+            int sno;
+            int max;
+            Console.Write("Enter First Number: ");
+            fno = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second number: ");
+            sno = int.Parse(Console.ReadLine());
+
+            if (fno > sno)
+                max = fno;
+            else
+                max = sno;
+            Console.WriteLine($"The max of {fno} and {sno} is {max}");
+
+        }
+    }
+}
