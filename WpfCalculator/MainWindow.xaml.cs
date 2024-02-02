a3b1158daa9baa932ae1a528e4759158aba519b8
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-02-02 17:36:07 +0530

    wpf sample code

---
+++ WpfCalculator/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace WpfCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int fno = int.Parse(txtFirst.Text);
+            int sno = int.Parse(txtSecond.Text);
+            int result = fno + sno;
+            MessageBox.Show($"The Sum of {fno} and {sno} is {result}");
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            int fno = int.Parse(txtFirst.Text);
+            int sno = int.Parse(txtSecond.Text);
+            int result = fno - sno;
+            MessageBox.Show($"The Diffrence of {fno} and {sno} is {result}");
+        }
+
+        private void button_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            int fno = int.Parse(txtFirst.Text);
+            int sno = int.Parse(txtSecond.Text);
+            int result = fno * sno;
+            MessageBox.Show($"The Product of {fno} and {sno} is {result}");
+        }
+
+        private void button_Copy2_Click(object sender, RoutedEventArgs e)
+        {
+            int fno = int.Parse(txtFirst.Text);
+            int sno = int.Parse(txtSecond.Text);
+            int result = fno / sno;
+            MessageBox.Show($"The Division of {fno} and {sno} is {result}");
+        }
+    }
+}
