7ee6f5952c9e4bdd78decee4d37dad24df81cc9d
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-02-05 17:30:09 +0530

    delegates demo

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace DelegatesDemo
+{
+
+    //class MyDelegate : Delegate
+    //{
+
+    //}
+    // Step 1: Delegate Declaration
+    delegate void MyDelegate(string str);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass a = new MyClass();
+            //a.RunDelete();
+            // invoke
+            a.MyDelegate.Invoke("Hello calling from program class");
+        }
+
+
+    }
+
+    class MyClass
+    {
+        // instantiate
+        public event MyDelegate MyDelegate = null;
+        public MyClass()
+        {
+            // initialize
+            MyDelegate = new MyDelegate(Greeting);
+        }
+        static void Greeting(string text)
+        {
+            Console.WriteLine($"Greeting: {text}");
+        }
+
+        // invoke
+        public void RunDelete()
+        {
+
+            MyDelegate("hello");
+        }
+    }
+}
---
+++ DelegatesDemo2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+
+namespace DelegatesDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // client 1
+            ProcessManager.ShowProcessList(p => true);
+
+            // client 2
+
+            //ProcessManager.ShowProcessList(FilterByName);
+
+            // Anonymous Delegates
+            ProcessManager.ShowProcessList(delegate (Process process)
+            {
+                return process.ProcessName.StartsWith("S");
+            });
+
+            // Lambda statments
+            ProcessManager.ShowProcessList((Process process) =>
+            {
+                return process.ProcessName.StartsWith("S");
+            });
+
+            // Lambda Expressions
+            ProcessManager.ShowProcessList((Process process) =>
+
+                 process.ProcessName.StartsWith("S")
+            );
+
+            // Lambda Expressions
+            ProcessManager.ShowProcessList(process => process.ProcessName.StartsWith("S"));
+
+
+            // client 3
+            //ProcessManager.ShowProcessList(FilterBySize);
+
+        }
+
+        // client 1
+        public static bool FilterByNone(Process process) { return true; }
+
+        // client 2
+        public static bool FilterByName(Process process)
+        {
+            return process.ProcessName.StartsWith("S");
+        }
+
+
+        // client 3
+        public static bool FilterBySize(Process p)
+        {
+            return p.WorkingSet64 > 100 * 1024 * 1024;
+        }
+    }
+
+
+    public delegate bool FilterDelegate(Process p);
+
+
+    // Backend code - 
+    class ProcessManager
+    {
+        //public static void ShowProcessList()
+        //{
+        //    foreach (var p in Process.GetProcesses())
+        //    {
+        //        Console.WriteLine(p.ProcessName);
+        //    }
+        //}
+
+        //public static void ShowProcessList(string sw)
+        //{
+        //    foreach (var p in Process.GetProcesses())
+        //    {
+        //        if (p.ProcessName.StartsWith(sw))
+        //            Console.WriteLine(p.ProcessName);
+        //    }
+        //}
+
+        public static void ShowProcessList(FilterDelegate filter)
+        {
+            foreach (var p in Process.GetProcesses())
+            {
+                if (filter(p))
+                    Console.WriteLine(p.ProcessName);
+            }
+        }
+    }
+}
