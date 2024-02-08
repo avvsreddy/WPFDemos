eec5c2d7682c37b127ce8fb611439be5d98c0386
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-02-08 17:56:57 +0530

    styles demo

---
+++ NotepadPlus/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace NotepadPlus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // exit the application
+            if (MessageBox.Show("Do you wish to exit the application?", "Application Exit", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                this.Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            // file save
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+        }
+    }
+}
